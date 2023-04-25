327cb2bcaf9d3bb5adeaf86924a897b2a31f63d7
Aydot <ayomidotzee@gmail.com>
2023-04-25 11:42:31 -0700

    April 25 1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,18 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const logger = require('./logger')
+const authorize = require('./authorize')
+
+app.use([logger, authorize])
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page')
+});
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5000...')
+});
---
+++ 02-express-tutorial/authorize.js
@@ -0,0 +1,19 @@
+const authorize = (req, res, next) => {
+    const { user } = req.query;
+    if(user === 'john'){
+        req.user = {
+            name: 'john', 
+            id : 3
+        }
+        console.log(req.query)
+        console.log('authorize')
+        next();
+    }
+
+    else {
+        res.status(401).send('unauthorized')
+        console.log('unauthorized')
+    }
+};
+
+module.exports = authorize
